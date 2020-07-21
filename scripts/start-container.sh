270ddbd101f97021ff31348adaaabc02250294c2
Martin D'Souza <martin@clarifit.com>
2020-07-21 08:41:37 -0600

    minor: echo TNS_ADMIN when set

--- scripts/start-container.sh
+++ scripts/start-container.sh
@@ -3,6 +3,8 @@
 if [ -z "$TNS_ADMIN" ]; then       
   echo "warning: \$TNS_ADMIN is empty, unsetting it"
   unset TNS_ADMIN
+else
+  echo "TNS_ADMIN: $TNS_ADMIN"
 fi
 
 # Call SQLcl with all the parameters used when calling sqlcl
